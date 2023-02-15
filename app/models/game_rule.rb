20305f59532f29818e88fc03be42f1b698760837
Dito Janelidze <ditojanelidze28@gmail.com>
2023-02-16 02:19:11 +0400

    create seeds for game start preperation

--- app/models/game_rule.rb
+++ app/models/game_rule.rb
@@ -1,4 +1,9 @@
 class GameRule < ApplicationRecord
   belongs_to :player_choice, class_name: 'Choice'
   belongs_to :computer_choice, class_name: 'Choice'
+
+  enum result: {
+    lose: 0,
+    win: 1
+  }
 end
