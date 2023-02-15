4a80276a2270baeed080c190a5f976f233e3c462
Dito Janelidze <ditojanelidze28@gmail.com>
2023-02-16 02:36:04 +0400

    Generate swagger and rspec gems to create documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   get 'choices', to: 'games#choices'
   post 'play', to: 'games#play'
 end
