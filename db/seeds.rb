91e61943b8b458918dab1b755226b9e186455d1e
Dito Janelidze <ditojanelidze28@gmail.com>
2023-02-16 03:00:09 +0400

    Finish general logic of the game, add forgotten tie rules

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,14 @@ scissor = Choice.find_or_create_by(id_name: :scissor)
 
 GameRule.find_or_create_by(player_choice: rock, computer_choice: paper, result: :lose)
 GameRule.find_or_create_by(player_choice: rock, computer_choice: scissor, result: :win)
+
 GameRule.find_or_create_by(player_choice: paper, computer_choice: scissor, result: :lose)
 GameRule.find_or_create_by(player_choice: paper, computer_choice: rock, result: :win)
+
 GameRule.find_or_create_by(player_choice: scissor, computer_choice: paper, result: :win)
 GameRule.find_or_create_by(player_choice: scissor, computer_choice: rock, result: :lose)
+
+# Ties
+GameRule.find_or_create_by(player_choice: rock, computer_choice: rock, result: :tie)
+GameRule.find_or_create_by(player_choice: scissor, computer_choice: scissor, result: :tie)
+GameRule.find_or_create_by(player_choice: paper, computer_choice: paper, result: :tie)
