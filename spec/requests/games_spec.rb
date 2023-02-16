04685a85b4a0e542073e0e0e9d25f3c6d64722db
Dito Janelidze <ditojanelidze28@gmail.com>
2023-02-16 06:02:54 +0400

    Fix failing swagger test

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -28,6 +28,16 @@ RSpec.describe 'employee', type: :request do
       }
 
       response '200', 'ok' do
+        before do
+          @choice1 = FactoryBot.create(:choice)
+          @choice2 = FactoryBot.create(:choice)
+          FactoryBot.create(:game_rule, :win_type, player_choice: @choice1, computer_choice: @choice2)
+          stub_request(:get, External::CurbService.url).to_return(
+            body: { statusCode: 200, body: @choice2.id_name }.to_json.to_s, status: 200
+          )
+        end
+
+        let(:params) { { player_choice: @choice1.id_name } }
         run_test!
       end
     end
